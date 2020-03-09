808dbc7da8ce31d5d650aea25dc4174fad1ed7ef
Dmitriy Kievskiy <dmitriy.kievskiy@iqoption.com>
2020-03-10 01:34:02 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/beevik/ntp"
+	"github.com/spf13/cast"
+)
+
 func main() {
-	// Place your code here
+	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		panic("Can not get exact time")
+	}
+	currentTime := time.Now()
+	fmt.Println("current time: " + cast.ToString(currentTime))
+	fmt.Println("exact time: " + cast.ToString(exactTime))
 }
