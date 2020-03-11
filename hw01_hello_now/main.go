7981e5a01febc659b83401cc8de757d83d37cba5
Dmitriy Kievskiy <dmitriy.kievskiy@iqoption.com>
2020-03-11 12:39:38 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/beevik/ntp"
-	"github.com/prometheus/common/log"
 )
+
+const timeFormat = "2006-01-02 15:04:05 -0700 MST"
 
 func main() {
 	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
@@ -14,6 +16,6 @@ func main() {
 		log.Fatal("Can not get exact time")
 	}
 	currentTime := time.Now()
-	fmt.Println("current time: " + currentTime.Format("2006-01-02 15:04:05 -0700 MST"))
-	fmt.Println("exact time: " + exactTime.Format("2006-01-02 15:04:05 -0700 MST"))
+	fmt.Println("current time: " + currentTime.Format(timeFormat))
+	fmt.Println("exact time: " + exactTime.Format(timeFormat))
 }
