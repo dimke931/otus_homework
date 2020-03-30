93323e6ae5641053b5842bac2082a5a0fd5d16f3
Dmitriy Kievskiy <dmitriy.kievskiy@iqoption.com>
2020-03-30 22:34:26 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -9,10 +9,6 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func IsEscaped(){
-
-}
-
 func Unpack(str string) (string, error) {
 	var result strings.Builder
 	var runes = []rune(str)
