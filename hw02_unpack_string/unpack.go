b32296b82e8724568c6a0f8e83f37eb54409ba37
Dmitriy Kievskiy <dmitriy.kievskiy@iqoption.com>
2020-03-30 22:06:21 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,31 @@ package hw02_unpack_string //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here
-	return "", nil
+
+func Unpack(str string) (string, error) {
+	var result strings.Builder
+	var runes = []rune(str)
+	for i, v := range runes {
+		if unicode.IsDigit(runes[0]) {
+			return "", ErrInvalidString
+		}
+		if !unicode.IsDigit(v) {
+			result.WriteRune(v)
+		} else {
+			digit, _ := strconv.Atoi(string(v))
+			if digit != 0 {
+				result.WriteString(strings.Repeat(string(runes[i-1]), digit-1))
+			} else {
+				return "", ErrInvalidString
+			}
+		}
+	}
+	return result.String(), nil
 }
